12ba3197d5d2dcfbe19941706e6866f62f053792
k2h2j3 <k2h2j3@gmail.com>
2023-07-25 13:33:52 +0900

    django accountapp 회원가입기능

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.http import HttpResponse, HttpResponseRedirect
+from accountapp.views import hello_world, AccountCreateView
 
-from accountapp.views import hello_world
+app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 
 def hello_world(request):
     return render(request, 'accountapp/hello_world.html')
+
+class AccountCreateView(CreateView):
+    model = User #연결할모델
+    form_class = UserCreationForm #로그인폼
+    success_url = reverse_lazy('accountapp:hello_world') #로그인성공시 연결할url
+    template_name = 'accountapp/create.html'
