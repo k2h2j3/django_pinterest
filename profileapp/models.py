ae6110dab6dafe1a00e077e7c7d5ee6668416a4c
k2h2j3 <k2h2j3@gmail.com>
2023-07-25 16:25:34 +0900

    django Profileapp modelForm 구현

---
+++ profileapp/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile') #유저가 탈퇴하면 프로필도 사라짐
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True)
+    message = models.CharField(max_length=100, null=True)
+
