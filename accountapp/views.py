dd3fc611cb771264dcc88f9cee0815ff34625389
k2h2j3 <k2h2j3@gmail.com>
2023-07-25 15:59:27 +0900

    django login decorators 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,15 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 
 
+has_ownership = [account_ownership_required, login_required]
+
+@login_required
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
 
 class AccountCreateView(CreateView):
     model = User #연결할모델
@@ -17,11 +24,14 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world') #로그인성공시 연결할url
     template_name = 'accountapp/create.html'
 
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user' #다른 사람들의 정보를 볼 수 있게 타겟을 지정
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -29,6 +39,8 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
