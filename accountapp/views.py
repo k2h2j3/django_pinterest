8b153819a0d4944248622dcdd21f22477c4198eb
k2h2j3 <k2h2j3@gmail.com>
2023-07-26 15:57:33 +0900

    django projectapp Detail페이지 Mixin적용및 연결

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,10 +6,11 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
-
+from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
 
@@ -25,10 +26,16 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user' #다른 사람들의 정보를 볼 수 있게 타겟을 지정
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
